7aa2be4d12d4a11f50e6298222844cf13f6323a4
Minjeong Bak <doniminp@gmail.com>
2022-04-17 18:19:43 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(navbarHeight);
+  //   console.log(window.scrollY);
+  //   console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
@@ -27,8 +27,15 @@ navbarMenu.addEventListener('click', (event) => {
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', (event) => {
+homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transparent as the window scrolls down.
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 function scrollIntoView(selector) {
